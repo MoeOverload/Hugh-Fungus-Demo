d95a24fbb13728f7301d309041a657dc1867fcf3
morgan <goombaplayz@gmail.com>
2025-10-03 06:25:15 -0700

    worked on enemy state machine
    still need attack state

--- hugh-fungus/enemy.gd
+++ hugh-fungus/enemy.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 var player_detected = false
 var direction
 var player
-var SPEED = 150
-
-
+var SPEED = 70
+var random_direction
+ 
 enum state {
 	IDLE,
 	CHASE,
@@ -12,7 +12,10 @@ enum state {
 	ATTACK,
 	}
 @onready var current_state = state.IDLE
-
+@onready var idle_timer : Timer =  $idle_timer
+@onready var wander_timer : Timer = $rand_Move_Timer
+func _ready() -> void:
+	idle_timer.start()
 func _physics_process(delta: float) -> void:
 	match current_state:
 		state.IDLE:
@@ -23,35 +26,63 @@ func _physics_process(delta: float) -> void:
 			handle_attack(delta)
 		state.CHASE:
 			handle_chase(delta)
-
+	move_and_slide()
 	
 	
 		
 	
 func handle_idle(_delta):
 	velocity = Vector2.ZERO
-	move_and_slide()
+	
+	
 
 func handle_chase(_delta):
 	if player_detected == true:
 		direction =  (player.global_position - self.global_position).normalized()
 		velocity = direction * SPEED
-	move_and_slide()
+	else:
+		velocity = Vector2.ZERO
+		current_state = state.IDLE
 
 func handle_wander(_delta):
-	if player_detected == false:
-		direction = (random_direction.global_position - self.global_position).normalized()
-
-
+	if player_detected:
+		current_state = state.CHASE
+		return
+	
+	velocity = random_direction * SPEED
+	
 
 func randomize_direction():
+	
+	var x = randf_range(-1.0,1.0)
+	var y = randf_range(-1.0,1.0)
+	random_direction = Vector2(x,y).normalized()
+
+
+func handle_attack(_delta):
+	velocity = Vector2.ZERO
+	#TODO
+
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_detected = true
-		player = body 
+		player = body
+		current_state = state.CHASE 
 		
 func _on_player_detection_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		player_detected = false
 		player = null
+		current_state = state.IDLE
+	
+
+
+func _on_rand_move_timer_timeout() -> void:
+	current_state = state.IDLE
+	idle_timer.start()
+
+func _on_idle_timer_timeout() -> void:
+	current_state = state.WANDER
+	randomize_direction()
+	wander_timer.start()
